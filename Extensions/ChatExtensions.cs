d0b858e0d5a079fd20cc3be12c0195488dec8b5d
Andrew Edmonds <Andy@thinkbase.ai>
2025-04-20 03:40:59 +0100

    refactoring

---
+++ Extensions/ChatExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.FluentUI.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ChatServiceCollectionExtensions
+    {
+        public static IServiceCollection AddBlazorChat(this IServiceCollection services)
+        {
+            services.AddFluentUIComponents();
+            services.AddBlazorAdaptiveCards();
+            return services;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This is a blazor component that implements a chat conversation, consuming an IChatClient as defined in [Microsoft.Extensions.ai](https://learn.microsoft.com/en-us/dotnet/ai/ai-extensions).
 It is based on the Chat code in [this example](https://devblogs.microsoft.com/dotnet/announcing-dotnet-ai-template-preview1/).
 This component can display responses that are plain text, Markdown and Adaptive Cards.
+
+
